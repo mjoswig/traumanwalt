68220988e023da56e41566e095ca959a982300de
Manuel Joswig <manuel@joswigsolutions.com>
2023-09-02 16:09:18 +0200

    Update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex justify-center">
-    <div class="p-4 sm:p-8 wrapper flex flex-col space-y-12 w-full">
+    <div class="p-4 sm:p-8 wrapper flex flex-col space-y-4 md:space-y-12 w-full">
       <header>
         <div class="flex items-center justify-between">
           <div>
-            <nuxt-link to="/"><img class="h-10 md:h-12 w-auto" src="@/assets/images/traumanwalt-logo-2.png" /></nuxt-link>
+            <nuxt-link to="/"><img class="h-10 md:h-12 w-auto" src="@/assets/images/traumanwalt-logo.png" /></nuxt-link>
           </div>
           <div>
             <div class="hidden md:flex items-center space-x-6 sm:space-x-8 sm:text-xl">
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="text-center">
-    <h1 class="text-3xl mb-4">Hier finden Sie bald den Anwalt Ihrer Träume!</h1>
-    <p>Bitte besuchen Sie uns in Kürze wieder. Diese Seite befindet sich derzeit noch im Aufbau.</p>
+  <div>
+    <div class="bg-gray-100 flex flex-col-reverse md:flex-row items-center shadow-md">
+      <div class="px-4 py-6 md:p-8 w-full">
+        <h1 class="mb-2">Finden Sie Ihren Traumanwalt</h1>
+        <p class="text-xl">Unser Ziel ist es, Ihnen mit nur wenigen Mausklicks den perfekten Fachanwalt für Ihre individuellen Bedürfnisse zu präsentieren.</p>
+      </div>
+      <div class="bg-cover h-56 sm:h-72 md:h-96 w-full" :style="`background-image: url(${require('@/assets/images/traumanwalt-home.jpeg')}); background-position: center 0; min-width: 40vw;`">
+      </div>
+    </div>
   </div>
 </template>
 

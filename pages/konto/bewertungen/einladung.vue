b515a29269abdcb41581de0adbf616ba04238623
Manuel Joswig <manuel@joswigsolutions.com>
2023-09-09 01:00:27 +0200

    Update

---
+++ pages/konto/bewertungen/einladung.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="mb-6">Zu Bewertung einladen</h1>
+    <AccountSection heading="Bewertungslink teilen" class="mb-4">
+    </AccountSection>
+    <AccountSection heading="Einladung per E-Mail versenden" class="mb-4">
+    </AccountSection>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KontoBewertungenEinladungPage'
+}
+</script>
---
+++ pages/konto/bewertungen/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="flex flex-col-reverse sm:flex-row sm:items-center sm:justify-between sm:space-y-0 mb-6">
+      <h1>Bewertungen</h1>
+      <div class="flex justify-end mb-4 sm:mb-0">
+        <Btn @click="$router.push('/konto/bewertungen/einladung')">Mandanten einladen</Btn>
+      </div>
+    </div>
+    <AccountSection>
+      Sie haben noch keine Bewertungen erhalten.
+    </AccountSection>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KontoBewertungenIndexPage'
+}
+</script>

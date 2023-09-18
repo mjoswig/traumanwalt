23205855146530cc5dce2449756951153783569f
Manuel Joswig <manuel@joswigsolutions.com>
2023-09-18 15:29:15 +0200

    Update

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -19,7 +19,10 @@
                   <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
                 </svg>
               </div>
-              <span>{{ phoneNumber }}</span>
+              <span>
+                <a v-show="showPhoneNumber" :href="phoneNumberUri">{{ nationalPhoneNumber }}</a>
+                <a v-show="!showPhoneNumber" class="cursor-pointer" @click="showPhoneNumber = true">Telefonnummer anzeigen</a>
+              </span>
             </div>
             <div class="flex space-x-2">
               <div>
@@ -47,7 +50,7 @@
             <h3 class="mb-4">Rechtsgebiete</h3>
             <p v-if="!legalFields.length">{{ firstName }} {{ lastName }} hat keine Rechtsgebiete angegeben.</p>
             <div v-if="legalFields.length" class="flex flex-wrap text-sm md:text-base">
-              <span class="bg-gray-100 px-2 py-1 rounded-md mr-1 mt-1 md:mr-2 md:mt-2" v-for="(legalField, index) in legalFields" :key="index">
+              <span class="break-keep bg-gray-100 px-2 py-1 rounded-md mr-1 my-1 md:mr-2 md:my-2" v-for="(legalField, index) in legalFields" :key="index">
                 {{ getLegalFieldName(legalField, profile) }}
               </span>
             </div>
@@ -56,7 +59,7 @@
             <h3 class="mb-4">Sprachen</h3>
             <p v-if="!languages.length">{{ firstName }} {{ lastName }} hat keine Sprachen angegeben.</p>
             <div v-if="languages.length" class="flex flex-wrap text-sm md:text-base">
-              <span class="bg-gray-100 px-2 py-1 rounded-md mr-1 mt-1 md:mr-2 md:mt-2" v-for="(language, index) in languages" :key="index">
+              <span class="break-keep bg-gray-100 px-2 py-1 rounded-md mr-1 my-1 md:mr-2 md:my-2" v-for="(language, index) in languages" :key="index">
                 {{ language.name }}
               </span>
             </div>
@@ -97,6 +100,8 @@
 </template>
 
 <script>
+import parsePhoneNumber from 'libphonenumber-js'
+
 export default {
   name: 'ProfilePage',
   head() {
@@ -108,7 +113,8 @@ export default {
     const profile = await app.$axios.$get(`/api/profiles/${params.slug}`)
     if (!profile) redirect('/anwaelte')
     return {
-      profile
+      profile,
+      showPhoneNumber: false
     }
   },
   computed: {
@@ -132,6 +138,12 @@ export default {
     },
     phoneNumber() {
       return this.profile.landline_number || this.profile.mobile_number
+    },
+    phoneNumberUri() {
+      return parsePhoneNumber(this.phoneNumber).getURI()
+    },
+    nationalPhoneNumber() {
+      return parsePhoneNumber(this.phoneNumber).formatNational()
     },
     mailToLink() {
       const subject = 'Kontaktanfrage über Traumanwalt'

3e97c3f517637f747038110c933761e0c5edf88b
Manuel Joswig <manuel@joswigsolutions.com>
2023-09-18 11:33:30 +0200

    Update

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="-mt-4">
-    <div class="bg-gray-100 p-8 flex flex-col items-center justify-center space-y-6">
-      <img class="bg-cover border h-36 w-36 md:h-48 md:w-48 rounded-full" :style="`background-image: url(${photoUrl});`" />
-      <div>
-        <span class="block text-gray-500 text-center text-lg xl:text-xl mb-1">{{ jobTitle }}</span>
+    <div class="relative bg-cover p-4 md:p-8 flex flex-col items-center justify-center space-y-3 md:space-y-6 rounded-md" :style="`background-image: url(${require('@/assets/images/traumanwalt-profile-cover.jpeg')});`">
+      <div class="absolute top-0 left-0 h-full w-full opacity-40 bg-black rounded-md"></div>
+      <img class="z-10 bg-cover border border-white h-36 w-36 md:h-48 md:w-48 rounded-full" :style="`background-image: url(${photoUrl});`" />
+      <div class="z-10 text-center text-white">
+        <span class="block text-lg xl:text-xl mb-1">{{ jobTitle }}</span>
         <h1>{{ profile.first_name }} {{ profile.last_name }}</h1>
       </div>
     </div>

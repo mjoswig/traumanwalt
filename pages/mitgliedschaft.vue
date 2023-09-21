3241ebd5f5b9569e5d954e01608a51b3dd6ab264
Manuel Joswig <manuel@joswigsolutions.com>
2023-09-21 18:14:50 +0800

    Update

--- pages/mitgliedschaft.vue
+++ pages/mitgliedschaft.vue
@@ -50,41 +50,6 @@
       </div>
       <div class="flex flex-col space-y-2 lg:space-y-0 lg:flex-row lg:space-x-4 border bg-white p-4 rounded-md shadow-md">
         <div class="lg:pt-1">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#daba6b" class="bi bi-question-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
-          </svg>
-        </div>
-        <div>
-          <h3 class="text-lg mb-1">Rechtsfragen beantworten</h3>
-          <p>Über Traumanwalt können Mandanten Ihnen Rechtsfragen stellen. Verdienen Sie 100 € pro beantworteter Frage!</p>
-        </div>
-      </div>
-      <div class="flex flex-col space-y-2 lg:space-y-0 lg:flex-row lg:space-x-4 border bg-white p-4 rounded-md shadow-md">
-        <div class="lg:pt-1">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#daba6b" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
-            <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
-            <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
-          </svg>
-        </div>
-        <div>
-          <h3 class="text-lg mb-1">Verträge rechtlich prüfen</h3>
-          <p>Mandanten können diverse Verträge bei uns prüfen lassen. Verdienen Sie 80% vom Nettopreis jeder Rechtsberatung!</p>
-        </div>
-      </div>
-      <div class="flex flex-col space-y-2 lg:space-y-0 lg:flex-row lg:space-x-4 border bg-white p-4 rounded-md shadow-md">
-        <div class="lg:pt-1">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#daba6b" class="bi bi-pencil" viewBox="0 0 16 16">
-            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-          </svg>
-        </div>
-        <div>
-          <h3 class="text-lg mb-1">Rechtstipps</h3>
-          <p>Veröffentlichen Sie Fachbeiträge zu Ihren Schwerpunkten und erlangen Sie so mehr Sichtbarkeit für Ihr Anwaltsprofil.</p>
-        </div>
-      </div>
-      <div class="flex flex-col space-y-2 lg:space-y-0 lg:flex-row lg:space-x-4 border bg-white p-4 rounded-md shadow-md">
-        <div class="lg:pt-1">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#daba6b" class="bi bi-star" viewBox="0 0 16 16">
             <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
           </svg>
@@ -107,6 +72,17 @@
       </div>
       <div class="flex flex-col space-y-2 lg:space-y-0 lg:flex-row lg:space-x-4 border bg-white p-4 rounded-md shadow-md">
         <div class="lg:pt-1">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#daba6b" class="bi bi-pencil" viewBox="0 0 16 16">
+            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+          </svg>
+        </div>
+        <div>
+          <h3 class="text-lg mb-1">Rechtstipps</h3>
+          <p>Veröffentlichen Sie Fachbeiträge zu Ihren Schwerpunkten und erlangen Sie so mehr Sichtbarkeit für Ihr Anwaltsprofil.</p>
+        </div>
+      </div>
+      <div class="flex flex-col space-y-2 lg:space-y-0 lg:flex-row lg:space-x-4 border bg-white p-4 rounded-md shadow-md">
+        <div class="lg:pt-1">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#daba6b" class="bi bi-building" viewBox="0 0 16 16">
             <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1ZM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Z"/>
             <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V1Zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3V1Z"/>
@@ -115,6 +91,30 @@
         <div>
           <h3 class="text-lg mb-1">Kostenloses Kanzleiprofil</h3>
           <p>Mehr als ein Anwaltsprofil? Kein Problem – mit unserem Kanzleiprofil, welches Sie selbst verwalten können.</p>
+        </div>
+      </div>
+      <div class="flex flex-col space-y-2 lg:space-y-0 lg:flex-row lg:space-x-4 border bg-white p-4 rounded-md shadow-md">
+        <div class="lg:pt-1">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#daba6b" class="bi bi-question-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+            <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
+          </svg>
+        </div>
+        <div>
+          <h3 class="text-lg mb-1"><span class="text-gray-400">Bald verfügbar:</span> Rechtsfragen beantworten</h3>
+          <p>Über Traumanwalt können Mandanten Ihnen Rechtsfragen stellen. Verdienen Sie 100 € pro beantworteter Frage!</p>
+        </div>
+      </div>
+      <div class="flex flex-col space-y-2 lg:space-y-0 lg:flex-row lg:space-x-4 border bg-white p-4 rounded-md shadow-md">
+        <div class="lg:pt-1">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#daba6b" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+            <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+            <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+          </svg>
+        </div>
+        <div>
+          <h3 class="text-lg mb-1"><span class="text-gray-400">Bald verfügbar:</span> Verträge rechtlich prüfen</h3>
+          <p>Mandanten können diverse Verträge bei uns prüfen lassen. Verdienen Sie 80% vom Nettopreis jeder Rechtsberatung!</p>
         </div>
       </div>
     </div>
--- pages/rechtstipps/_slug.vue
+++ pages/rechtstipps/_slug.vue
@@ -11,15 +11,15 @@
             <div class="bg-cover bg-center border h-12 w-12 sm:h-16 sm:w-16 rounded-full" :style="`background-image: url(${legalGuide.user_photo_url  || require('@/assets/images/photo-default.jpeg')});`"></div>
           </div>
           <div>
-            <nuxt-link :to="`/${legalGuide.user_slug}`"><b>{{ getFullName }}</b></nuxt-link>
+            <nuxt-link :to="`/${legalGuide.user_slug}`"><b>{{ fullName }}</b></nuxt-link>
             <span class="block text-gray-500 text-sm">Veröffentlicht: {{ $moment(legalGuide.created_at).format('DD.MM.YYYY, HH:mm')  }}</span>
           </div>
         </div>
         <div v-html="legalGuide.content">
         </div>
       </article>
-      <div class="w-full lg:w-1/3">
-        <div class="sticky top-4 border border-gray-300 p-4 rounded-md">
+      <div class="hidden lg:block w-full lg:w-1/3">
+        <!-- div class="sticky top-4 border border-gray-300 p-4 rounded-md">
           <h2 class="text-xl xl:text-2xl mb-2">Sie haben eine Rechtsfrage?</h2>
           <p class="text-lg mb-3">So einfach geht's auf Traumanwalt:</p>
           <ol class="flex flex-col space-y-2 list-decimal ml-5 mb-5">
@@ -29,8 +29,21 @@
             <li>Hilfe bekommen</li>
           </ol>
           <Btn class="w-full" @click="$router.push('/rechtsberatung')">Jetzt beraten werden</Btn>
+        </div !-->
+        <div class="sticky top-4 border border-gray-300 p-4 rounded-md">
+          <h2 class="text-xl xl:text-2xl mb-2">Nachricht an {{ salutation }}{{ salutation === 'Herr' ? 'n' : '' }} {{ lastName }}</h2>
+          <form class="flex flex-col space-y-4" @submit.prevent>
+            <fieldset>
+              <label>Ihre Nachricht</label>
+              <textarea class="border px-2 py-1 rounded-md w-full" rows="5" placeholder="Bitte beschreiben Sie hier Ihre Situation bzw. Ihren rechtlichen Beratungsbedarf möglichst genau..." v-model="message" />
+            </fieldset>
+            <Btn @click="processMessage">Nachricht senden</Btn>
+          </form>
         </div>
       </div>
+      <nuxt-link :to="`/${userSlug}/nachricht`" class="mobile-cta lg:hidden fixed bottom-0 left-0 font-bold shadow-md px-4 py-3 flex justify-center focus:outline-none w-full z-20">
+        Nachricht an {{ salutation }}{{ salutation === 'Herr' ? 'n' : '' }} {{ lastName }}
+      </nuxt-link>
     </div>
   </div>
 </template>
@@ -47,11 +60,21 @@ export default {
     const legalGuide = await app.$axios.$get(`/api/legal-guides/${params.slug}`)
     if (!legalGuide) redirect('/rechtstipps')
     return {
-      legalGuide
+      legalGuide,
+      message: ''
     }
   },
   computed: {
-    getFullName() {
+    userSlug() {
+      return this.legalGuide.user_slug
+    },
+    salutation() {
+      return this.legalGuide.user_salutation
+    },
+    lastName() {
+      return this.legalGuide.user_last_name
+    },
+    fullName() {
       let fullName = []
       if (this.legalGuide.user_salutation === 'Frau') {
         fullName.push('Rechtsanwältin')
@@ -62,6 +85,22 @@ export default {
       fullName.push(this.legalGuide.user_last_name)
       return fullName.join(' ')
     }
+  },
+  methods: {
+    processMessage() {
+      this.$router.push(`/${this.legalGuide.user_slug}/nachricht?message=${this.message}`)
+    }
   }
 }
 </script>
+
+<style lang="postcss" scoped>
+.mobile-cta {
+  @apply text-white;
+  background: #222222;
+
+  &:hover {
+    @apply text-white no-underline;
+  }
+}
+</style>

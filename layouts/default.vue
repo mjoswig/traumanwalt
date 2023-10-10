71086ea80f746901ceabbacba9360a3e51beaf59
Manuel Joswig <manuel@joswigsolutions.com>
2023-10-10 10:20:04 +0800

    Update

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,6 @@
                 <div class="hidden md:flex items-center space-x-6 xl:space-x-8 xl:text-lg">
                   <nuxt-link to="/anwaelte" class="hidden lg:inline-block">Anwälte finden</nuxt-link>
                   <nuxt-link to="/rechtstipps" class="hidden lg:inline-block">Rechtstipps</nuxt-link>
-                  <nuxt-link to="/jobs" class="hidden lg:inline-block">Jobbörse</nuxt-link>
                   <nuxt-link v-show="!$store.getters.isLoggedIn" to="/login">Login</nuxt-link>
                   <nuxt-link v-show="$store.getters.isLoggedIn" to="/konto/logout">Logout</nuxt-link>
                   <Btn v-show="!isLoggedIn" @click="$router.push('/mitgliedschaft')">Sie sind Anwalt?</Btn>
@@ -40,7 +39,6 @@
             <div class="bg-gray-100 p-4 flex-col space-y-4 rounded-md shadow-sm mt-4" :class="{ 'flex lg:hidden': showMobileMenu, 'hidden': !showMobileMenu }">
               <nuxt-link to="/anwaelte">Anwälte finden</nuxt-link>
               <nuxt-link to="/rechtstipps">Rechtstipps</nuxt-link>
-              <nuxt-link to="/jobs">Jobbörse</nuxt-link>
               <nuxt-link v-show="!isLoggedIn" to="/login" class="md:hidden">Login</nuxt-link>
               <nuxt-link v-show="isLoggedIn" to="/konto/logout" class="md:hidden">Logout</nuxt-link>
               <Btn v-show="!isLoggedIn" class="w-fit md:hidden" @click="$router.push('/mitgliedschaft')">Sie sind Anwalt?</Btn>

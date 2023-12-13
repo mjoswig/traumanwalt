6ffefc391cb7f8762ec266cde94418fbfcd081ce
Manuel Joswig <manuel@joswigsolutions.com>
2023-12-13 16:14:48 +0800

    Update

--- pages/gesetze/index.vue
+++ pages/gesetze/index.vue
@@ -34,7 +34,7 @@ export default {
   async asyncData({ app }) {
     const laws = await app.$axios.$get(`/api/laws`)
     return {
-      letters: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+      letters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
       laws
     }
   }

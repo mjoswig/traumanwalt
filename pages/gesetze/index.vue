5838bb214a090105522d1170ec6a55ae66528cd4
Manuel Joswig <manuel@joswigsolutions.com>
2023-12-13 20:29:52 +0800

    Update

--- pages/gesetze/index.vue
+++ pages/gesetze/index.vue
@@ -5,14 +5,14 @@
       <p class="text-xl">Traumanwalt hat die wichtigsten Gesetzestexte für Sie auf dieser Seite zusammengefasst.</p>
     </div>
     <section class="flex flex-col space-y-2 md:space-y-4">
-      <h2>Meistgesuchte Gesetze</h2>
+      <h2 class="block pb-2 pt-2 md:pt-0">Meistgesuchte Gesetze</h2>
       <div v-for="(law, index) in laws.filter(l => l.popular)" :key="index">
         &rightarrow; <b>{{ law.title_short }}</b> – {{ law.title_long }}
       </div>
-      <h2 class="pt-4">Alphabetische Gesetzesübersicht</h2>
+      <h2 class="pt-5 md:pt-10">Alphabetische Gesetzesübersicht</h2>
       <div  class="flex flex-col space-y-2 md:space-y-4" v-for="(letter, index) in letters" :key="index">
-        <h3>{{ letter }}</h3>
-        <div v-for="(law, index) in laws.filter(law => law.title_short.startsWith(letter))" :key="index">
+        <h3 class="block md:pb-2 pt-4">{{ letter }}</h3>
+        <div class="md:pb-2" v-for="(law, index) in laws.filter(law => law.title_short.startsWith(letter))" :key="index">
           &rightarrow; <b>{{ law.title_short }}</b> – {{ law.title_long }}
         </div>
       </div>

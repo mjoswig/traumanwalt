f41be7bd09802b997148add4e2513ecd2dcf3310
Manuel Joswig <manuel@joswigsolutions.com>
2024-01-05 13:39:47 +0800

    Update

--- pages/rechtsberatung/_slug/index.vue
+++ pages/rechtsberatung/_slug/index.vue
@@ -15,17 +15,15 @@
       </svg>
       <nuxt-link :to="`/rechtsberatung/${legalService.slug}`">{{ legalService.name }}</nuxt-link>
     </div>
-    <section class="flex flex-col space-y-6 lg:flex-row lg:space-x-8 lg:space-y-0 w-full">
-      <div class="w-full lg:w-2/3">
+    <article class="flex flex-col space-y-6 lg:flex-row lg:space-x-8 lg:space-y-0 w-full">
+      <section class="w-full lg:w-2/3">
         <h1>{{ legalService.name }}</h1>
         <p v-if="legalService.excerpt" class="text-lg md:text-xl mt-2 md:mt-4">{{ legalService.excerpt }}</p>
         <div v-if="legalService.thumbnail_url" class="bg-cover bg-center border rounded-lg h-48 sm:h-64 md:h-72 xl:h-96 w-full mt-4 md:mt-7" :style="`background-image: url(${legalService.thumbnail_url});`"></div>
-      </div>
-      <div class="w-full lg:w-1/3">
-        <div class="sticky top-4 border border-gray-300 p-4 rounded-md">
+        <div class="border border-gray-300 p-4 rounded-md mt-6 lg:hidden">
           <h2 class="text-xl xl:text-2xl mb-2">{{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(legalService.price) }}</h2>
           <p class="mb-2">Rechtsprobleme sind eine enorme Belastung im Alltag. Lassen Sie sich die Last abnehmen.</p>
-          <p class="font-bold text-gray-500 text-lg mb-2">Vorteile der Traumanwalt-Beratung</p>
+          <p class="font-bold text-gray-500 text-lg mb-2">Schneller Recht bekommen</p>
           <ul class="flex flex-col space-y-1 mb-5">
             <li class="flex space-x-2">
               <div>
@@ -33,7 +31,7 @@
                   <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
                 </svg>
               </div>
-              <span>Schnelle Rechtsberatung zum Festpreis</span>
+              <span>Online-Rechtsberatung zum Festpreis</span>
             </li>
             <li class="flex space-x-2">
               <div>
@@ -54,8 +52,61 @@
           </ul>
           <Btn class="w-full" :is-disabled="true">Jetzt Beratung buchen</Btn>
         </div>
-      </div>
-    </section>
+        <h2 class="mt-4 md:mt-8 mb-2 md:mb-4">So funktioniert die Traumanwalt-Beratung</h2>
+        <div class="grid grid-cols gap-2 md:gap-4">
+          <div>
+            <h3 class="mb-2">1. Kontaktaufnahme</h3>
+            <p class="md:text-lg">Sie schildern uns Ihr Problem und senden uns alle relevanten Daten für die Beratung.</p>
+          </div>
+          <div>
+            <h3 class="mb-2">2. Bezahlung</h3>
+            <p class="md:text-lg">Sie bezahlen das Beratungshonorar in Höhe von {{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(legalService.price) }} in Vorkasse. Sie können mittels Visa, Mastercard, Apple Pay, Google Pay, PayPal, giropay, Sofort oder Klarna bezahlen.</p>
+          </div>
+          <div>
+            <h3 class="mb-2">3. Vermittlung an einen Traumanwalt</h3>
+            <p class="md:text-lg">Wir finden einen Anwalt, der Sie optimal beraten kann. Sie bekommen binnen 48 Stunden eine Rückmeldung von einem unserer Partneranwälte. Sollten wir nach 48 Stunden keinen Anwalt für Sie gefunden haben, bekommen Sie Ihr Geld vollständig erstattet.</p>
+          </div>
+          <div>
+            <h3 class="mb-2">4. Rechtsberatung</h3>
+            <p class="md:text-lg">Ihr Anwalt wird ein persönliches Gespräch mit Ihnen vereinbaren und Ihnen bei Ihrem Rechtsproblem helfen.</p>
+          </div>
+        </div>
+      </section>
+      <section class="hidden lg:block w-full lg:w-1/3">
+        <div class="sticky top-4 border border-gray-300 p-4 rounded-md">
+          <h2 class="text-xl xl:text-2xl mb-2">{{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(legalService.price) }}</h2>
+          <p class="mb-2">Rechtsprobleme sind eine enorme Belastung im Alltag. Lassen Sie sich die Last abnehmen.</p>
+          <p class="font-bold text-gray-500 text-lg mb-2">Schneller Recht bekommen</p>
+          <ul class="flex flex-col space-y-1 mb-5">
+            <li class="flex space-x-2">
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-lg mt-1" viewBox="0 0 16 16">
+                  <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                </svg>
+              </div>
+              <span>Online-Rechtsberatung zum Festpreis</span>
+            </li>
+            <li class="flex space-x-2">
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-lg mt-1" viewBox="0 0 16 16">
+                  <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                </svg>
+              </div>
+              <span>Transparente Kommunikation mit Fachanwälten auf Augenhöhe</span>
+            </li>
+            <li class="flex space-x-2">
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-lg mt-1" viewBox="0 0 16 16">
+                  <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                </svg>
+              </div>
+              <span>Sichere Übermittlung von vertraulichen Daten und Dokumenten</span>
+            </li>
+          </ul>
+          <Btn class="w-full" :is-disabled="true">Jetzt Beratung buchen</Btn>
+        </div>
+      </section>
+    </article>
   </div>
 </template>
 

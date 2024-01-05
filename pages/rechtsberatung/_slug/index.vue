612228c0bd74f56cb26a8f43edd706d56535fef1
Manuel Joswig <manuel@joswigsolutions.com>
2024-01-05 15:12:02 +0800

    Update

--- pages/rechtsberatung/_slug/index.vue
+++ pages/rechtsberatung/_slug/index.vue
@@ -13,15 +13,15 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill mr-2" viewBox="0 0 16 16">
         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
       </svg>
-      <nuxt-link :to="`/rechtsberatung/${legalService.slug}`">{{ legalService.name }}</nuxt-link>
+      <nuxt-link :to="`/rechtsberatung/${service.slug}`">{{ service.name }}</nuxt-link>
     </div>
     <article class="flex flex-col space-y-6 lg:flex-row lg:space-x-8 lg:space-y-0 w-full">
       <section class="w-full lg:w-2/3">
-        <h1>{{ legalService.name }}</h1>
-        <p v-if="legalService.excerpt" class="text-lg md:text-xl mt-2 md:mt-4">{{ legalService.excerpt }}</p>
-        <div v-if="legalService.thumbnail_url" class="bg-cover bg-center border rounded-lg h-48 sm:h-64 md:h-72 xl:h-96 w-full mt-4 md:mt-7" :style="`background-image: url(${legalService.thumbnail_url});`"></div>
+        <h1>{{ service.name }}</h1>
+        <p v-if="service.excerpt" class="text-lg md:text-xl mt-2 md:mt-4">{{ service.excerpt }}</p>
+        <div v-if="service.thumbnail_url" class="bg-cover bg-center border rounded-lg h-48 sm:h-64 md:h-72 xl:h-96 w-full mt-4 md:mt-7" :style="`background-image: url(${service.thumbnail_url});`"></div>
         <div class="border border-gray-300 p-4 rounded-md mt-6 lg:hidden">
-          <h2 class="text-xl xl:text-2xl mb-2">{{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(legalService.price) }}</h2>
+          <h2 class="text-xl xl:text-2xl mb-2">{{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(service.price) }}</h2>
           <p class="mb-2">Rechtsprobleme sind eine enorme Belastung im Alltag. Lassen Sie sich die Last abnehmen.</p>
           <p class="font-bold text-gray-500 text-lg mb-2">Schneller Recht bekommen</p>
           <ul class="flex flex-col space-y-1 mb-5">
@@ -60,7 +60,7 @@
           </div>
           <div>
             <h3 class="mb-2">2. Bezahlung</h3>
-            <p class="md:text-lg">Sie bezahlen das Beratungshonorar in Höhe von {{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(legalService.price) }} in Vorkasse. Sie können mittels Visa, Mastercard, Apple Pay, Google Pay, PayPal, giropay, Sofort oder Klarna bezahlen.</p>
+            <p class="md:text-lg">Sie bezahlen das Beratungshonorar in Höhe von {{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(service.price) }} in Vorkasse. Sie können mittels Visa, Mastercard, Apple Pay, Google Pay, PayPal, giropay, Sofort oder Klarna bezahlen.</p>
           </div>
           <div>
             <h3 class="mb-2">3. Vermittlung an einen Traumanwalt</h3>
@@ -71,10 +71,23 @@
             <p class="md:text-lg">Ihr Anwalt wird ein persönliches Gespräch mit Ihnen vereinbaren und Ihnen bei Ihrem Rechtsproblem helfen.</p>
           </div>
         </div>
+        <h2 class="mt-4 md:mt-8 mb-3 md:mb-6">Ähnliche Beratungsleistungen</h2>
+        <div class="grid grid-cols sm:grid-cols-2 xl:grid-cols-3 gap-3 sm:gap-6">
+          <div class="border flex flex-col justify-between rounded-md space-y-1" v-for="(relatedService, index) in relatedServices" :key="index">
+            <div>
+              <div class="h-40 bg-cover bg-center bg-no-repeat rounded-t-md mb-3" :style="`background-image: url(${relatedService.thumbnail_url});`"></div>
+              <h3 class="text-lg lg:text-xl px-4 hyphens-auto">{{ relatedService.name }}</h3>
+            </div>
+            <div class="px-4 pb-4">
+              <span class="block lg:text-lg mb-3">{{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(relatedService.price) }}</span>
+              <Btn class="w-full" @click="$router.push(`/rechtsberatung/${relatedService.slug}`)">Mehr erfahren</Btn>
+            </div>
+          </div>
+        </div>
       </section>
       <section class="hidden lg:block w-full lg:w-1/3">
         <div class="sticky top-4 border border-gray-300 p-4 rounded-md">
-          <h2 class="text-xl xl:text-2xl mb-2">{{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(legalService.price) }}</h2>
+          <h2 class="text-xl xl:text-2xl mb-2">{{ new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(service.price) }}</h2>
           <p class="mb-2">Rechtsprobleme sind eine enorme Belastung im Alltag. Lassen Sie sich die Last abnehmen.</p>
           <p class="font-bold text-gray-500 text-lg mb-2">Schneller Recht bekommen</p>
           <ul class="flex flex-col space-y-1 mb-5">
@@ -115,14 +128,15 @@ export default {
   name: 'RechtsberatungDetailsPage',
   head() {
     return {
-      title: `${this.legalService.name} - Traumanwalt`
+      title: `${this.service.name} - Traumanwalt`
     }
   },
   async asyncData({ app, params, redirect }) {
     const legalService = await app.$axios.$get(`/api/legal-services/${params.slug}`)
     if (!legalService) redirect('/rechtsberatung')
     return {
-      legalService
+      service: legalService.service,
+      relatedServices: legalService.related_services
     }
   },
   computed: {

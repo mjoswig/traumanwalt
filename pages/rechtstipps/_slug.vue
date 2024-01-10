f6fe572090e1416e155f4b0bd235b10d8583fe92
Manuel Joswig <manuel@joswigsolutions.com>
2024-01-10 22:37:41 +0800

    Update

--- pages/rechtstipps/_slug.vue
+++ pages/rechtstipps/_slug.vue
@@ -9,11 +9,11 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill mr-2" viewBox="0 0 16 16">
         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
       </svg>
-      <nuxt-link  class="mr-2" to="/rechtstipps">Rechtstipps</nuxt-link>
+      <nuxt-link class="mr-2" to="/rechtstipps">Rechtstipps</nuxt-link>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill mr-2" viewBox="0 0 16 16">
         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
       </svg>
-      <nuxt-link  class="mr-2" :to="`/${legalGuide.user_slug}/rechtstipps`">{{ fullNameWithoutJobTitle }}</nuxt-link>
+      <nuxt-link class="mr-2" :to="`/${legalGuide.user_slug}/rechtstipps`">{{ fullNameWithoutJobTitle }}</nuxt-link>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill mr-2" viewBox="0 0 16 16">
         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
       </svg>

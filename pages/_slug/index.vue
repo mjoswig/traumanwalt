6893fd9552787fedf089854d1045b1b9625dcaef
Manuel Joswig <manuel@joswigsolutions.com>
2024-01-12 12:00:33 +0800

    Update

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -50,7 +50,7 @@
           </svg>
         </a>
       </div>
-      <nuxt-link v-if="profile.fake" :to="`/${profile.slug}/profil-melden`" class="report-profile-link flex items-center space-x-2 text-sm text-white z-10">
+      <nuxt-link v-if="!profile.fake" :to="`/${profile.slug}/profil-melden`" class="report-profile-link flex items-center space-x-2 text-sm text-white z-10">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
           <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
         </svg>
@@ -234,7 +234,10 @@
           <div class="flex justify-center w-full mb-3">
             <div class="profile-photo bg-cover h-32 w-32 rounded-full" :style="`background-image: url(${photoUrl});`"></div>
           </div>
-          <h2 class="text-center text-xl xl:text-2xl mb-4">{{ firstName }} {{ lastName }}</h2>
+          <h2 class="text-center text-xl xl:text-2xl mb-2">{{ firstName }} {{ lastName }}</h2>
+          <div class="flex justify-center mb-4">
+            <star-rating class="mb-1" :increment="0.1" :read-only="true" :show-rating="false" :star-size="30" v-model="averageRating" />
+          </div>
           <form class="flex flex-col space-y-3" @submit.prevent>
             <Btn @click="processMessage">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
